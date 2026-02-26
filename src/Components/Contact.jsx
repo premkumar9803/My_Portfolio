d3963ae8d22bb588fb18a56a3716f0b9d4416dba
premkumar9803 <premsiddhu26@gmail.com>
2026-02-26 22:55:23 +0530

    second commit

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './Contact.css'
 import github from '../assets/Image/github (1).png'
 import linkedin from '../assets/Image/linkedin (1).png'
 
@@ -14,7 +15,7 @@ export default function Contact() {
             Contact <span className=" text-[#0ef]">Me!</span>
           </h2>
         
-          <div className=" mt-2 md:ml-96 md:mt-5 md:text-lg text-white opacity-85">
+          <div className=" mt-2 px-3 md:ml-96 md:mt-5 md:text-lg text-white opacity-85">
             <div className=" flex items-center gap-1.5">
               <i className="fa-solid fa-envelope text-yellow-500"></i>
               <a href="">premsiddhu26@gmail.com</a>
@@ -31,11 +32,11 @@ export default function Contact() {
 
           <hr className=" mt-5 opacity-45" />
           <div className=" flex items-center md:justify-between"> 
-            <p className=" mt-3 opacity-65 text-xs text-white md:ml-120 md:text-sm">
+            <p className=" md:ml-120 md:text-sm copy">
               Copyright ©2026 by Prem Kumar | All Rights Reserved.</p>
             <div className="">
-              <a href="#Home"><i className="fa-solid fa-arrow-up text-sm mt-4 bg-[#0ef] text-[#323946] 
-              rounded-md p-2 px-3 pr-5 hover:shadow-[0_0_1rem_#0ef] transition ease-in hover:scale-105 md:mr-8">
+              <a href="#Home"><i className="fa-solid fa-arrow-up
+               hover:shadow-[0_0_1rem_#0ef] transition ease-in hover:scale-105 md:mr-8 arrow">
             </i></a>
             </div>
           </div>  
